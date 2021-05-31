1b4ce6001c11bbe49dc97bf708405c9b0ef71c70
Nischay J <nischay193@gmail.com>
2021-05-31 23:03:56 +0530

    Add styles and keyboard listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+var fieldText = "";
 
 function eval_string(str) {
     try {
@@ -8,17 +8,16 @@ function eval_string(str) {
     }
     return str;
 }
-var fieldText = "";
 function handleClick(event) {
     var symbol = event.target.innerText;
     switch (symbol) {
-        case "Back":
+        case "⌫︁":
             fieldText = fieldText.slice(0, -1);
             break;
         case "C":
             fieldText = "";
             break;
-        case "Ans":
+        case "=":
             fieldText = eval_string(fieldText);
             break;
         default:
@@ -27,10 +26,45 @@ function handleClick(event) {
     }
     const field = document.getElementById("field");
     field.value = fieldText;
-    console.log(fieldText);
 }
+
+function keyboardListener(event) {
+    var symbol = event.key;
+    switch (symbol) {
+        case "Backspace":
+            fieldText = fieldText.slice(0, -1);
+            break;
+        case "Enter":
+            fieldText = eval_string(fieldText);
+            break;
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+        case "/":
+        case "*":
+        case "-":
+        case "+":
+            fieldText += symbol;
+            break;
+    }
+    const field = document.getElementById("field");
+    field.value = fieldText;
+}
+
 const buttons = document.querySelectorAll("button");
 
+// add button listeners
 buttons.forEach(button => {
     button.addEventListener("click", handleClick);
-})
+});
+
+// Keyboard Listeners
+document.addEventListener("keydown", keyboardListener);
+
